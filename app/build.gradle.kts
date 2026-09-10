cfe7b5808721917da077d6d8e8489a3ea8a02581
Nuutti Kotivuori <naked@iki.fi>
2026-09-10 12:42:27 +0000

    ci: handle absent release pull requests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     alias(libs.plugins.room3)
 }
 
-val agentknockVersionCode = 48
+val agentknockVersionCode = 49
 val agentknockVersionName =
     providers
         .fileContents(rootProject.layout.projectDirectory.file("version.txt"))
