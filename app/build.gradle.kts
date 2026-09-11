7d4b3fdf70303be8ea50022305b0c2b79a780592
Nuutti Kotivuori <naked@iki.fi>
2026-09-11 11:37:35 +0000

    docs: update release verification after repository transfer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     alias(libs.plugins.room3)
 }
 
-val agentknockVersionCode = 50
+val agentknockVersionCode = 51
 val agentknockVersionName =
     providers
         .fileContents(rootProject.layout.projectDirectory.file("version.txt"))
