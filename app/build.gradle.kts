0b17394243af5b2f91df55e3c958d5fbfbfbeba0
Nuutti Kotivuori <naked@iki.fi>
2026-09-14 09:44:43 +0000

    fix: give launcher icons more breathing room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     alias(libs.plugins.room3)
 }
 
-val agentknockVersionCode = 67
+val agentknockVersionCode = 68
 val agentknockVersionName =
     providers
         .fileContents(rootProject.layout.projectDirectory.file("version.txt"))
