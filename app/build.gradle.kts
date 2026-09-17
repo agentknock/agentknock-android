e8544a89e62668f0b6c59d28f09e6a3f4d5562ab
Nuutti Kotivuori <naked@iki.fi>
2026-09-17 13:20:23 +0000

    fix: show request processing without audible alerts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     alias(libs.plugins.room3)
 }
 
-val agentknockVersionCode = 78
+val agentknockVersionCode = 79
 val agentknockVersionName =
     providers
         .fileContents(rootProject.layout.projectDirectory.file("version.txt"))
